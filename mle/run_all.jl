a5b0d95fa622f1e13a2ea449b98325915ea129a3
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-05-18 13:18:51 -0400

    Update to work with Gen #417

--- mle/run_all.jl
+++ mle/run_all.jl
@@ -8,15 +8,15 @@ end
 
 load_generated_functions()
 
-init_param!(foo, :mu, -1)
+init_parameter!((foo, :mu), -1)
 
 trace, = generate(foo, (), choicemap((:y, 3)))
 step_size = 0.01
 for iter=1:1000
     accumulate_param_gradients!(trace, 0.)
-    grad_val = get_param_grad(foo, :mu)
-    set_param!(foo, :mu, get_param(foo, :mu) + step_size * grad_val)
-    zero_param_grad!(foo, :mu)
+    gradient = get_gradient((foo, :mu))
+    value = get_parameter_value((foo, :mu))
+    init_parameter!((foo, :mu), vlaue + step_size * gradient)
 end
 
 @assert abs(get_param(foo, :mu) - 3) < 1e-2
