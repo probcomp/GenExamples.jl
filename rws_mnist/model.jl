3941f4498dac42d3ff8546b79576040b8044e18d
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-05-19 16:53:06 -0400

    add note about issue

--- rws_mnist/model.jl
+++ rws_mnist/model.jl
@@ -17,36 +17,26 @@ using Gen: @gen, @param, @load_generated_functions
     @param x_W::Matrix{Float32}
     @param x_b::Vector{Float32}
 
-    # TODO issue -- Gen wraps h3_n in a ReverseDiff, which causes a failure 
-    h3_n = 10
-    #h3_n = length(h3_b)
-
-    #h2_n = length(h2_b)
-    #h1_n = length(h1_b)
-    #h1_n = 200
-    #x_n = length(x_b)
+    # NOTE: we should instead use h3_n = length(h3_b), but we can't because of
+    # https://github.com/probcomp/Gen.jl/issues/418
+    h3_n = 10 
 
     # sample third hidden layer (10 units)
     h3 ~ sigmoid_belief_network([false], h3_b, zeros(Float32, h3_n, 1))
 
     # sample second hidden layer (200 units)
-    #@assert size(h2_W) == (h2_n, h3_n) # TODO @assert is not supported in static modeling language
     h2 ~ sigmoid_belief_network(h3, h2_b, h2_W)
 
     # sample first hidden layer (200 units)
-    #@assert size(h1_W) == (h1_n, h2_n)
     h1 ~ sigmoid_belief_network(h2, h1_b, h1_W)
-    #h1 ~ sigmoid_belief_network([false], h1_b, zeros(Float32, h1_n, 1))
 
     # sample visible layer (28x28=784 units)
-    #@assert size(x_W) == (x_n, h1_n)
     x ~ sigmoid_belief_network(h1, x_b, x_W)
 
     return nothing
 end
 
 @gen (static) function q(x::Vector{Bool})
-    #@assert sum(x) > 0
 
     # first hidden layer
     @param h1_W::Matrix{Float32}
@@ -60,21 +50,13 @@ end
     @param h3_W::Matrix{Float32}
     @param h3_b::Vector{Float32}
 
-    #x_n = length(x)
-    #h1_n = length(h1_b)
-    #h2_n = length(h2_b)
-    #h3_n = length(h3_b)
-    
     # sample first hidden layer (200 units)
-    #@assert size(h1_W) == (h1_n, x_n)
     h1 ~ sigmoid_belief_network(x, h1_b, h1_W)
 
     # sample second hidden layer (200 units)
-    #@assert size(h2_W) == (h2_n, h1_n)
     h2 ~ sigmoid_belief_network(h1, h2_b, h2_W)
 
     # sample third hidden layer (10 units)
-    #@assert size(h3_W) == (h3_n, h2_n)
     h3 ~ sigmoid_belief_network(h2, h3_b, h3_W)
 
     return nothing
@@ -85,14 +67,16 @@ end
 import Random
 using Gen: apply_update!, init_parameter!, get_parameter_value
 
-# see https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/models/sbn.py#L94
-# and https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/model.py#L22
+# parameter initializations based on:
+# https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/models/sbn.py#L94
+# https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/model.py#L22
+# https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/models/sbn.py#L94
+
 function make_W(n_output, n_input)
     scale = Float32(sqrt(6f0) / sqrt(n_input + n_output))
     return scale * ((2*rand(Float32, n_output, n_input)) .- 1.0f0) / n_input
 end
 
-# see https://github.com/jbornschein/reweighted-ws/blob/e96414719d09ab4941dc77bab4cf4847acc6a8e7/learning/models/sbn.py#L94
 make_b(n) = -ones(Float32, n)
 
 function initialize_p_params!(; x_n=28*28, h1_n=200, h2_n=200, h3_n=10)
